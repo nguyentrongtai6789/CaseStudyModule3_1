6a780403ae54545e19de3c4955473e7f6c70c57b
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2023-08-30 15:47:55 +0700

    trieu da pull 3.48_30/08

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,7 +1,7 @@
 package model;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+//import javafx.application.Application;
+//import javafx.stage.Stage;
 
 public class Account {
     private int id;
